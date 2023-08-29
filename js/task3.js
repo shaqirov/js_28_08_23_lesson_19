4de2102b39b7c054ab35b08fca54c07e73289a31
Shakirov Azamat <shaqirov.a@gmail.com>
2023-08-29 19:04:46 +0600

    added task 3

--- js/task3.js
+++ js/task3.js
@@ -4,5 +4,33 @@
 По нажатию на кнопку "Добавить" введенный продукт должен быть добавлен к списку, отображаемому на странице.
 Используйте localStorage, чтобы сохранять список покупок между посещениями страницы.*/
 //======================================================================================================
+const form = document.forms.form
+const btn = document.getElementById('btn')
+const list = document.getElementById('list')
 
+function showList () {
+    const nameFromJSON = localStorage.getItem('product');
+    if (nameFromJSON) {
+        const productForList = JSON.parse(nameFromJSON);
+        if (productForList.product !== "") {
+            const li = document.createElement('li')
+            li.innerText = `${productForList.product}`;
+            list.append(li)
+        }
+    }
+}
+window.addEventListener('load', showList);
+
+function getProduct(event) {
+    event.preventDefault();
+
+    const {product} = form;
+    const productName = {
+        product: product.value
+    };
+    const productForJSON = JSON.stringify(productName);
+    localStorage.setItem('product', productForJSON);
+    showList()
+}
+form.addEventListener('submit', getProduct);
 //======================================================================================================
