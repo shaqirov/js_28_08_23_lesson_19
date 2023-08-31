bc451ee9f2944a08fe80b9b45a80cec99a681dc9
Shakirov Azamat <shaqirov.a@gmail.com>
2023-08-31 12:10:33 +0600

    Task 3

--- js/task3.js
+++ js/task3.js
@@ -4,33 +4,30 @@
 По нажатию на кнопку "Добавить" введенный продукт должен быть добавлен к списку, отображаемому на странице.
 Используйте localStorage, чтобы сохранять список покупок между посещениями страницы.*/
 //======================================================================================================
-const form = document.forms.form
+const addProduct = document.getElementById('addProduct')
 const btn = document.getElementById('btn')
-const list = document.getElementById('list')
-
-function showList () {
-    const nameFromJSON = localStorage.getItem('product');
-    if (nameFromJSON) {
-        const productForList = JSON.parse(nameFromJSON);
-        if (productForList.product !== "") {
-            const li = document.createElement('li')
-            li.innerText = `${productForList.product}`;
-            list.append(li)
-        }
-    }
-}
+const productsList = document.getElementById('list')
+const products = []
+btn.addEventListener('click', function(event) {
+    event.preventDefault()
+    let newProduct = addProduct.value
+    products.push(newProduct)
+    const productsForJSON = JSON.stringify(products);
+    localStorage.setItem('product', productsForJSON);
+    showList()
+})
 window.addEventListener('load', showList);
 
-function getProduct(event) {
-    event.preventDefault();
-
-    const {product} = form;
-    const productName = {
-        product: product.value
-    };
-    const productForJSON = JSON.stringify(productName);
-    localStorage.setItem('product', productForJSON);
-    showList()
+function showList () {
+    const productsFromJSON = localStorage.getItem('product');
+    const productsForList = JSON.parse(productsFromJSON);
+    let show = ''
+    for (let i = 0; i < productsForList.length; i++) {
+        const productForList = productsForList[i];
+        show += `
+            <li>${productForList}</li>
+        `
+        list.innerHTML = show
+    }
 }
-form.addEventListener('submit', getProduct);
-//======================================================================================================
+
